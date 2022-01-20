7b9ae16ad3f51aa4bba487ce69cf4b33374ce4a5
vvzvlad <vvzvlad@gmail.com>
2022-01-20 21:04:28 +0300

    update

--- app.py
+++ app.py
@@ -10,35 +10,45 @@
 
 import paho.mqtt.client as mqtt
 import time
+from locale import atof, setlocale, LC_NUMERIC
 
 import os
 import json
 import sys
 import random
 
-value = 0
+room_temp = 0
+local_temp = 0
+local_temp_cal = 0
+local_temp_cal_calc = 0
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
-  #client.subscribe("mqtt_helper/wakeup")
+  client.subscribe("zigbee/sensor_082a")
+  client.subscribe("zigbee/thermostat_0403")
   client.publish("mqtt_helper/status", payload="mqtt helper daemon started", qos=0, retain=False)
+  client.publish("zigbee/thermostat_0403/set", str("{\"eco_temperature\": 25}"), qos=0, retain=False)
 
 
 def on_message(client, userdata, msg):
-  print("Received MQTT message:" + msg.topic + ": " + str(msg.payload))
+  #print("Received MQTT message:" + msg.topic + ": " + str(msg.payload))
+  global room_temp, local_temp, local_temp_cal, local_temp_cal_calc
 
-  if msg.topic == "mqtt_helper/wakeup":
+  if msg.topic == "zigbee/sensor_082a":
     json_data = json.loads(msg.payload)
-    playlist_name = json_data.get('playlist')
-    device_name = json_data.get('device')
-    if playlist_name is not None and device_name is not None:
-      check_play_and_start_playlist(playlist_name, device_name)
+    room_temp = json_data.get('temperature')
+    print("temperature: " + str(room_temp))
+  if msg.topic == "zigbee/thermostat_0403":
+    json_data = json.loads(msg.payload)
+    local_temp = json_data.get('local_temperature')
+    local_temp_cal = json_data.get('local_temperature_calibration')
+    print("local temperature: " + str(local_temp) + ", cal: " + str(local_temp_cal))
 
 
 
 def main():
   counter = 0
-  period = 60*5
+  period = 60
   client = mqtt.Client()
   client.on_connect = on_connect
   client.on_message = on_message
@@ -46,11 +56,18 @@ def main():
   time.sleep(5)
   client.loop_start()
   while True:
-    uptime = counter * period
-    client.publish("mqtt_helper/status/uptime", str(uptime), qos=0, retain=False)
-    client.publish("zigbee/thermostat_0403/set", str("{\"eco_temperature\": 17}"), qos=0, retain=False)
     time.sleep(period)
+    uptime = counter * period
     counter = counter + 1
+    client.publish("mqtt_helper/status/uptime", str(uptime), qos=0, retain=False)
+
+    if room_temp != 0 and local_temp != 0:
+      current_local_temp_cal_calc = round(room_temp-local_temp, 1) + local_temp_cal
+      current_local_temp_cal_calc = str(current_local_temp_cal_calc)
+      print("current calc cal: " + current_local_temp_cal_calc + ", calc cal: " + str(round(room_temp-local_temp, 1)) + ", old cal: " + str(local_temp_cal) + ", local: " + str(local_temp) + ", room: " + str(room_temp))
+
+      client.publish("zigbee/thermostat_0403/set", str("{\"local_temperature_calibration\": " + current_local_temp_cal_calc + "}"), qos=0, retain=False)
+
   client.loop_stop()
 
 
